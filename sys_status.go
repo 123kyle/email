49c44e98e5483376bfd9fc2ff773253be1a00691
123kyle <KyledeMacBook-Pro.local>
2021-09-03 22:41:12 +0800

    update: diskusage

--- sys_status.go
+++ sys_status.go
@@ -1,7 +1,9 @@
 package email
 
 import (
+	"github.com/shirou/gopsutil/v3/disk"
 	"github.com/shirou/gopsutil/v3/load"
+	"os"
 )
 
 func CpuLoadStat() (load1, load5, load15 float64, err error) {
@@ -11,4 +13,16 @@ func CpuLoadStat() (load1, load5, load15 float64, err error) {
 		return -1, -1, -1, err
 	}
 	return avgLoadStat.Load1, avgLoadStat.Load5, avgLoadStat.Load15, nil
+}
+
+func DiskUsage() (float64, error) {
+	dir, err := os.Getwd()
+	if err != nil {
+		return -1, err
+	}
+	usage, err := disk.Usage(dir)
+	if err != nil {
+		return -1, err
+	}
+	return usage.UsedPercent, nil
 }
