17e3318c9f1dbb0283161108ca06a5deba6e11d5
wanyuetian <1041282946@qq.com>
2022-08-18 11:32:37 +0800

    Create isBinarySearchTree.go

---
+++ algo/isBinarySearchTree.go
@@ -0,0 +1,79 @@
+package algo
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+}
+
+func isBST1(head *Node) bool {
+	if head == nil {
+		return true
+	}
+	arr := make([]*Node, 0)
+	var in func(head *Node)
+	in = func(head *Node) {
+		if head == nil {
+			return
+		}
+		in(head.Left)
+		arr = append(arr, head)
+		in(head.Right)
+	}
+
+	in(head)
+	if len(arr) == 1 {
+		return true
+	}
+	for i := 1; i < len(arr); i++ {
+		if arr[i-1].Val >= arr[i].Val {
+			return false
+		}
+	}
+	return true
+}
+
+type Info struct {
+	IsB      bool
+	Min, Max int
+}
+
+func isBST2(head *Node) bool {
+	if head == nil {
+		return true
+	}
+	return process(head).IsB
+}
+
+func process(node *Node) *Info {
+	if node == nil {
+		return nil
+	}
+	leftInfo := process(node.Left)
+	rightInfo := process(node.Right)
+	if (leftInfo != nil && !leftInfo.IsB) || (rightInfo != nil && !rightInfo.IsB) {
+		return &Info{IsB: false}
+	}
+
+	if (leftInfo != nil && leftInfo.Max >= node.Val) || (rightInfo != nil && rightInfo.Min <= node.Val) {
+		return &Info{IsB: false}
+	}
+
+	minNum := node.Val
+	maxNum := node.Val
+	if leftInfo != nil {
+		minNum = min(leftInfo.Min, minNum)
+		maxNum = max(leftInfo.Max, maxNum)
+	}
+
+	if rightInfo != nil {
+		minNum = min(rightInfo.Min, minNum)
+		maxNum = max(rightInfo.Max, maxNum)
+	}
+
+	return &Info{
+		IsB: true,
+		Min: minNum,
+		Max: maxNum,
+	}
+}
